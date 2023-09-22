68c89187ea70b7cbbe3aaca521b8be1fc6bcdd57
canonallan <133584930+canonallan@users.noreply.github.com>
2023-09-22 15:40:18 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+
+value = input(f"Hello {name}, enter the first number: ")
+value1 = input("Enter the second number: ")
+
+value = int(value)
+value1 = int(value1)
+value2 = value + value1
+
+print(f"The sum of your numbers is {value2}")
+ 
+
